67998a3b35b1158e54bd397bdf20a203a519fe88
Alexander Städing <alexanderstaeding@gmail.com>
2023-05-04 10:48:17 +0200

    Initialize template

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 version = file("version").readLines().first()
 
 jagr {
-    assignmentId.set("h_id_")
+    assignmentId.set("h13")
     submissions {
         val main by creating {
             studentId.set("ab12cdef")
@@ -18,15 +18,15 @@ jagr {
     }
     graders {
         val graderPublic by creating {
-            graderName.set("_name_-Public")
-            rubricProviderName.set("h_id_.H_id__RubricProvider")
+            graderName.set("H13-Public")
+            rubricProviderName.set("h13.H13_RubricProvider")
             configureDependencies {
                 implementation(libs.algoutils.tutor)
             }
         }
         val graderPrivate by creating {
             parent(graderPublic)
-            graderName.set("_name_-Private")
+            graderName.set("H13-Private")
         }
     }
 }
@@ -38,7 +38,7 @@ dependencies {
 }
 
 application {
-    mainClass.set("h_id_.Main")
+    mainClass.set("h13.Main")
 }
 
 tasks {
